8128b7005179e0d7d51679019e5420262e0ad61f
Basit Adedeji <basitbriez@gmail.com>
2025-04-25 18:40:53 +0100

    Update user.py

--- BACKEND/app/schemas/user.py
+++ BACKEND/app/schemas/user.py
@@ -2,13 +2,29 @@ from pydantic import BaseModel, EmailStr
 from typing import Optional
 from datetime import datetime
 
+class EmailRequest(BaseModel):
+    email: str
+
+class UsernameRequest(BaseModel):
+    username: str
+
+class PasswordRequest(BaseModel):
+    password: str
+
 class UserBase(BaseModel):
     email: EmailStr
     full_name: str
     username: Optional[str] = None
 
 class UserCreate(UserBase):
+    username: str
+    email: EmailStr
+    postcode: str
+    DOB: str
+    address: str
+    full_name: str
     password: str
+
 
 class UserUpdate(BaseModel):
     email: Optional[EmailStr] = None
@@ -24,6 +40,9 @@ class UserResponse(UserBase):
     class Config:
         from_attributes = True
 
+class MessageResponse(BaseModel):
+    message: str
+    
 class Token(BaseModel):
     access_token: str
     token_type: str
