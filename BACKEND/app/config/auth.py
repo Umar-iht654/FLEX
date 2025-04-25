13283924e181099d2c5c8c82242204ec9c3f4242
Basit Adedeji <basitbriez@gmail.com>
2025-04-25 18:41:38 +0100

    Update auth.py

--- BACKEND/app/config/auth.py
+++ BACKEND/app/config/auth.py
@@ -2,11 +2,12 @@ from datetime import timedelta
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
-from app.core.security import create_access_token, verify_password
+from app.core.security import create_access_token, verify_password, get_password_hash
 from app.db.session import get_db
 from FLEX.backend.app.models.user import User
-from app.schemas.user import UserCreate, UserResponse, Token
+from app.schemas.user import UserCreate, EmailRequest, UsernameRequest,PasswordRequest, MessageResponse, Token
 from app.config.settings import get_settings
+import re
 
 router = APIRouter()
 
@@ -52,19 +53,48 @@ async def login(
     )
     return {"access_token": access_token, "token_type": "bearer"}
 
-@router.post("/register", response_model=UserResponse)
-async def register(user: UserCreate, db: Session = Depends(get_db)):
-    db_user = db.query(User).filter(User.email == user.email).first()
+@router.post("/checkEmail", response_model=MessageResponse)
+async def checkEmail(email_req: EmailRequest, db: Session = Depends(get_db)):
+    db_user = db.query(User).filter(User.email == email_req.email).first()
     if db_user:
         raise HTTPException(
             status_code=400,
             detail="Email already registered"
         )
+    return {"message": "email is available"}
     
+@router.post("/checkUsername", response_model=MessageResponse)
+async def checkUsername(username_req: UsernameRequest, db: Session = Depends(get_db)):
+    db_user = db.query(User).filter(User.username == username_req.username).first()
+    if db_user:
+        raise HTTPException(
+            status_code=400,
+            detail="Username already taken"
+        )
+    return {"message": "username is available"}
+
+@router.post("/checkPassword", response_model=MessageResponse)
+async def checkPassword(password_req: PasswordRequest, db: Session = Depends(get_db)):
+    password_pattern = r"^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"
+    if not re.match(password_pattern, password_req.password):
+        raise HTTPException(
+            status_code=400,
+            detail="Password does not meet the required criteria. It must contain at least one uppercase letter, one digit, and one special character (@$!%*?&) and be at least 8 characters long."
+        )
+    
+    return {"message": "password is valid"}
+
+@router.post("/register", response_model=MessageResponse)
+async def register(user: UserCreate, db: Session = Depends(get_db)):
+    secured_password = get_password_hash(user.password)
     new_user = User(
         email=user.email,
         username=user.username,
-        hashed_password=user.password  # Password hashing should be done in the model
+        hashed_password=secured_password,  # Password hashing should be done in the model
+        postcode=user.postcode,
+        address=user.address,
+        DOB=user.DOB,
+        fullname=user.full_name
     )
     db.add(new_user)
     db.commit()
