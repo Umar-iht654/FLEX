f33f4efb98125daaa8c8f04ffc4f2c7f514b6992
Basit Adedeji <Basitbriez@gmail.com>
2025-05-01 00:47:17 +0100

    id unno

--- FrontEnd/screens/GroupProfilePage.js
+++ FrontEnd/screens/GroupProfilePage.js
@@ -63,9 +63,6 @@ const GroupProfilePage = ({ navigation, route}) => {
     async function UploadPageInfo() {
         try {
             const response = await axios.post('https://390d-138-253-184-53.ngrok-free.app/groupProfile', {user_usn: user.username ,user2_usn:group });
-
-        try {
-            const response = await axios.post('https://390d-138-253-184-53.ngrok-free.app/groupProfile', {user_usn: user.username ,user2_usn:group });
             if(response.data && response.data.message) {
                 const newGroupInfo = {
                     username: response.data.group.group_name,
